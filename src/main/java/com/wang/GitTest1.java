a3dc888beaabf53f5d97e187dd07ca015d08accd
WangTian <1026595185@qq.com>
2021-06-07 20:41:16 +0800

    hot-fix commit

--- src/main/java/com/wang/GitTest1.java
+++ src/main/java/com/wang/GitTest1.java
@@ -4,5 +4,6 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("Hello git1!");
         System.out.println("Hello git2!");
+        System.out.println("Hello hot-fix!");
     }
 }
