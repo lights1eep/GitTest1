cdde7c5c04b8d367cfe6da0a185e6f1bc59fb719
WangTian <1026595185@qq.com>
2021-06-07 20:32:05 +0800

    second commit

--- src/main/java/com/wang/GitTest1.java
+++ src/main/java/com/wang/GitTest1.java
@@ -3,5 +3,6 @@ package com.wang;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("Hello git1!");
+        System.out.println("Hello git2!");
     }
 }
